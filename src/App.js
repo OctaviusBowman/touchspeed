8b17c87b87e3ccc6f6fcc337ed461515f37a6e3e
OctaviusBowman <octaviusbowman@gmail.com>
2020-10-18 17:49:14 -0400

    favicon added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Preview from './Preview'
 import Speed from './Speed'
 import getPrompt from './getPrompt'
+import Leaderbord from './Leaderboard'
 
 const initialState = {
   text: getPrompt(),
@@ -43,28 +44,32 @@ const App = () => {
   }
 
   return (
-    <div className="container mb-5 mt-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <Preview text={state.text} userInput={state.userInput} />
-          <textarea
-            value={state.userInput}
-            onChange={event => setState({ ...state, userInput: event.target.value, symbols: correctCount(event.target.value), started: true, finished: isDone(event.target.value) })}
-            className="form-control mb-3"
-            placeholder="Start Typing..."
-            readOnly={state.finished || state.sec >= 60}
-          >
-          </textarea>
-          <div className="text-left">
-            <button className="btn btn-light" onClick={() => window.location.reload(false)}>New Prompt</button>
-          </div>
-          <Speed sec={state.sec} symbols={state.symbols} text={state.text} input={state.userInput} />
-          <div className="text-right">
-            <button className="btn btn-light" onClick={() => setState(initialState)}>Restart</button>
+    <div className="container">
+      <div className="mb-5 mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <Preview text={state.text} userInput={state.userInput} />
+            <textarea
+              value={state.userInput}
+              onChange={event => setState({ ...state, userInput: event.target.value, symbols: correctCount(event.target.value), started: true, finished: isDone(event.target.value) })}
+              className="form-control mb-3"
+              placeholder="Start Typing..."
+              readOnly={state.finished || state.sec >= 60}
+            >
+            </textarea>
+            <div className="text-left">
+              <button className="btn btn-light" onClick={() => window.location.reload()}>New Prompt</button>
+            </div>
+            <Speed sec={state.sec} symbols={state.symbols} text={state.text} input={state.userInput} />
+            <div className="text-right">
+              <button className="btn btn-light" onClick={() => setState(initialState)}>Restart</button>
+            </div>
           </div>
         </div>
-      </div>
-    </div >
+      </div >
+      <Leaderbord />
+    </div>
+
   );
 }
 
---
+++ src/Leaderboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Leaderboard = () => {
+    return (
+        <div className="leaderboard">
+
+            <div className="leaderboard-title">
+                Leaderboard
+            </div>
+            <div className="leaderboard-body">
+                List of Leaders
+                will go in this
+                column
+            </div>
+        </div>
+    )
+}
+
+export default Leaderboard
