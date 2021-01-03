0cd56ebc6693e3327423182022a1cc3ecb3485b2
OctaviusBowman <octaviusbowman@gmail.com>
2021-01-03 18:43:04 -0500

    WPM bug fix

--- src/Speed.js
+++ src/Speed.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Speed = ({ sec, symbols, text, input}) => {
+const Speed = ({ sec, symbols, text, input }) => {
     if (symbols !== 0 && sec !== 0) {
-        let wpm = (text.split(' ').length) / (sec / 60)
+        let wpm = ((input.split(' ').length) / (sec / 60))
 
         // Calculate's the users speed after a minute of typing
         if (sec >= 60) {
