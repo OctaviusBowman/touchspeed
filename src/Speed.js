3dbe35137f3031e4626f1825faa371ca87c7229f
OctaviusBowman <octaviusbowman@gmail.com>
2021-01-04 22:06:46 -0500

    Filtered WPM [TODO]: Handle edge cases for filtering words

--- src/Speed.js
+++ src/Speed.js
@@ -6,7 +6,19 @@ const Speed = ({ sec, symbols, text, input }) => {
 
         // Calculate's the users speed after a minute of typing
         if (sec >= 60) {
-            wpm = input.split(' ').length;
+            // Correct amout of word's typed
+            let inputSplit = input.split(' ')
+            let textSplit = text.split(' ')
+
+            //TODO: Add logic that compares textSplit length and inputSplit length at each value mathces, if they aren't another
+            // filter comparison is needed that checks the character index's [indexOf()]
+            let filteredText = inputSplit.filter((word, i) => {
+                return word === textSplit[i]
+            })
+        
+
+            // return <div className="wpm">{filteredText.length} WPM with {inputSplit.length - filteredText.length} errors</div>
+            return <div className="wpm">{filteredText.length} WPM</div>
         }
 
         return (
